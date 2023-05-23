0b466348b7353cd4c2fbb20e60cb10efb409cf2d
W.Zheng <weiningzheng@outlook.com>
2023-05-23 23:43:40 +0800

    first upload

---
+++ hello_me.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+df = pd.DataFrame({'Hersteller':['BMW','VW','PORSCHE','AUDI'], 'Preis':[100,200,300,400],
+'Location':['München','Wolfsburg','Stuttgart','Ingostadt']})
+
+print (df)
